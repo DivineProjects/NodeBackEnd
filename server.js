d6bc55f8b8e83ffeb1cad64e844d7db18dadfbbb
divineProjects <divinejigu@gmail.com>
2025-11-05 01:22:41 +0200

    updated database

--- server.js
+++ server.js
@@ -17,12 +17,17 @@ const static = require("./routes/static")
  *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
-app.set("layout", "./layouts/layouts")
+app.set("layout", "./layouts/layout")
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+
+// Index route
+app.get("/", function(req, res) {
+  res.render("index", { title: "Home" })
+})
 
 /* ***********************
  * Local Server Information
